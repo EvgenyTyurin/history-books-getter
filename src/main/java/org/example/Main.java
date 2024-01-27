59b4b3dc6bf8c97b39197702c12e7f84644afe89
EvgenyTyurin <beshere@yandex.ru>
2024-01-27 16:24:53 +0300

    Add book cover image names

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,27 +16,48 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        // Читаем файл от Озона
         var content = Files.readString(Path.of("in.txt"));
 
-        var booksStr = content.split("&quot;textAtom&quot;:\\{&quot;text&quot;:&quot;");
-        var books = Arrays.stream(booksStr)
+        // Создаем записи о книгах
+        var bookStrings = content.split("&quot;textAtom&quot;:\\{&quot;text&quot;:&quot;");
+        bookStrings = ArrayUtils.remove(bookStrings, 0);
+
+        // Создаем коллекцию книг по записям
+        var books = Arrays.stream(bookStrings)
                 .map(str -> str.split("&")[0])
                 .map(str -> {
                     var titles = str.split(" \\| ");
                     return new Book(titles[0], titles.length > 1 ? titles[1] : "");
                 })
                 .collect(Collectors.toList());
-        books.remove(0);
 
+        // Проставляем обложки книг
+        for (int i =0; i < books.size(); i++) {
+            var imageName = bookStrings[i].split("\\.jpg")[0];
+            var strArray = imageName.split("u002");
+            imageName = strArray[strArray.length - 1];
+            if ("Fcover".equals(imageName)) {
+                imageName = bookStrings[i].split("\\.jpg")[1];
+                strArray = imageName.split("u002");
+                imageName = strArray[strArray.length - 1];
+            }
+            books.get(i).setCoverImageName(imageName.split("F")[1]);
+        }
+
+        // Готовим json-писатель
         var objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
         var json = objectWriter.writeValueAsString(books);
 
-        Files.writeString(Path.of("out.txt"), json);
+        // Пишем json файл
+        Files.writeString(Path.of("out.json"), json);
     }
 
     static class Book {
         String title;
         String authors;
+        String coverImageName;
 
         public Book(String title, String authors) {
             this.title = title;
@@ -56,6 +78,14 @@ public class Main {
 
         public void setAuthors(String authors) {
             this.authors = authors;
+        }
+
+        public String getCoverImageName() {
+            return coverImageName;
+        }
+
+        public void setCoverImageName(String coverImageName) {
+            this.coverImageName = coverImageName;
         }
     }
 }
